df8ada9ca4a1a4263cb391e0f6bd2851324a25be
rvdforst <rolf.v.d.forst@fau.de>
2021-10-26 15:08:35 +0200

    Create UsersList.php

---
+++ includes/UsersList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace RRZE\SSO;
+
+defined('ABSPATH') || exit;
+
+class UsersList
+{
+    public function onLoaded()
+    {
+        add_filter('manage_users_columns', [$this, 'attributes']);
+        add_action('manage_users_custom_column', [$this, 'attributesColumns'], 10, 3);
+        add_filter('wpmu_users_columns', [$this, 'attributes']);
+        add_action('wpmu_users_custom_column', [$this, 'attributesColumns'], 10, 3);
+    }
+
+    public function attributes($columns)
+    {
+        $columns['attributes'] = __('Attributes', 'rrze-sso');
+        return $columns;
+    }
+
+    public function attributesColumns($value, $columnName, $userId)
+    {
+        if ('attributes' != $columnName) {
+            return $value;
+        }
+
+        $attributes = [];
+
+        $eduPersonAffiliation = get_user_meta($userId, 'edu_person_affiliation', true);
+        if ($eduPersonAffiliation) {
+            $attributes[] = is_array($eduPersonAffiliation) ? implode('<br>', $eduPersonAffiliation) : $eduPersonAffiliation;
+        }
+
+        $eduPersonEntitlement = get_user_meta($userId, 'edu_person_entitlement', true);
+        if ($eduPersonEntitlement) {
+            $attributes[] = is_array($eduPersonEntitlement) ? implode('<br>', $eduPersonEntitlement) : $eduPersonEntitlement;
+        }
+
+        return implode('<br>', $attributes);
+    }
+}

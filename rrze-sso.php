92facb972b522a1c0dff27973a17301de7fca181
rvdforst <rolf.v.d.forst@fau.de>
2023-04-12 09:33:19 +0200

    Update rrze-sso.php

--- rrze-sso.php
+++ rrze-sso.php
@@ -4,7 +4,7 @@
 Plugin Name:      RRZE SSO
 Plugin URI:       https://github.com/RRZE-Webteam/rrze-sso
 Description:      Single-Sign-On (SSO) SAML-Integrations-Plugin für WordPress.
-Version:          1.5.6
+Version:          1.6.0
 Author:           RRZE-Webteam
 Author URI:       https://blogs.fau.de/webworking/
 License:          GNU General Public License v2
@@ -126,6 +126,19 @@ function plugin()
 }
 
 /**
+ * Instantiate SimpleSAML class.
+ * @return object SimpleSAML
+ */
+function simpleSAML()
+{
+    static $instance;
+    if (null === $instance) {
+        $instance = new SimpleSAML();
+    }
+    return $instance;
+}
+
+/**
  * Execute on 'plugins_loaded' API/action.
  * @return void
  */
@@ -153,6 +166,6 @@ function loaded()
         });
         return;
     }
-    $main = new Main;
-    $main->onLoaded();
+    $main = new Main();
+    $main->loaded();
 }
